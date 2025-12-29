5bfc9a63565886fb1ed224d355c014f813aabe0b
Charlie Blaker <charlie3blaker@gmail.com>
2025-12-29 01:56:25 -0500

    First Commit

---
+++ build/CMakeFiles/VulkanTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanTest.dir/src/main.cpp.obj"
+  "CMakeFiles/VulkanTest.dir/src/main.cpp.obj.d"
+  "VulkanTest.exe"
+  "VulkanTest.exe.manifest"
+  "VulkanTest.pdb"
+  "libVulkanTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vkApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vkApp.dir/src/main.cpp.obj"
+  "CMakeFiles/vkApp.dir/src/main.cpp.obj.d"
+  "libvkApp.dll.a"
+  "vkApp.exe"
+  "vkApp.exe.manifest"
+  "vkApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vkApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
